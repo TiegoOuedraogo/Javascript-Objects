9c39757d64f16e56fd1ab3e53eea067eea835a80
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2023-12-04 09:29:38 -0500

    getLearnerData func implemented

--- script.js
+++ script.js
@@ -3,156 +3,164 @@
 •	name: Name of the course ("Introduction to JavaScript").
 */
 const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
-  /**•	id: Numeric ID of the assignment group (12345).
+  id: 451,
+  name: "Introduction to JavaScript",
+};
+
+/**•	id: Numeric ID of the assignment group (12345).
 •	name: Name of the assignment group ("Fundamentals of JavaScript").
 •	course_id: ID of the associated course (451).
 •	group_weight: Weight of the assignment group as a percentage (25%).
 •	assignments: Array of assignments, each with an ID, name, due date, and points possible.
 */
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  /**LearnerSubmissions: Array of objects representing submissions by learners.
-   * Each object includes the learner ID, assignment ID, 
-   * and submission details (submission date and score).
-*/
-  const LearnerSubmissions = [
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
     {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
     },
     {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
     },
     {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
     },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
+  ],
+};
+
+/**LearnerSubmissions: Array of objects representing submissions by learners.
+ * Each object includes the learner ID, assignment ID,
+ * and submission details (submission date and score).
+ */
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47,
     },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
-    }
-  ];
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
+];
 
-  const isLate=(submittedAt, dueAt)=>{
-    const submittedDate = new Date(submittedAt);
-    const dueDate = new Date(dueAt);
-
-    try {
-        if(isNaN(submittedDate.getTime()) || isNaN(dueDate.getTime)){
-        }
-    } catch (error) {
-        console.error("error: ", error.message);
-    }
-    return submittedDate > dueDate
-
-  }
-
-const calculateScore=(submission,assignment,latePenalty)=>{
-    const maxScore = assignment.points_possible;
-    let score = submission.score;
-    if(latePenalty){
-        score = Math.max(score -maxScore* 0.1,0)
-    }
-
-    return score/maxScore
-  }
-
-  const validateCourseAssignment=( course, ag)=>{
-    if(course.id !== ag.course.id){
-        throw new Error("Wrong Assignment group");
-    }
-    console.error.log("validation passed")
-  } 
-  
-  const learnerEachSubmission = (learnerId) => {
-    LearnerSubmissions.forEach(submission => {
-      if (submission.learner_id === learnerId) {
-        /** find method returns the value of the first element in the array where predicate is true, and undefined otherwise.
-         * find calls predicate once for each element of the array, in ascending order, 
-         * until it finds one where predicate returns true. If such an element is found, 
-         * find immediately returns that element value. Otherwise, find returns undefined. */
-        const assignment = AssignmentGroup.assignments.find(assignment => assignment.id === submission.assignment_id);
-
-        if (assignment) {
-          const late = isLate(submission.submission.submitted_at,assignment.due_at);
-          console.log(`Assignment: ${assignment.name}, Submitted At: ${submission.submission.submitted_at}, Due At: ${assignment.due_at}, Late: ${late}`);
-        }
-      }
-
-    
-    });
-  };
-  LearnerSubmissions.forEach(submission => {
-    const assignment = AssignmentGroup.assignments.find(a => a.id === submission.assignment_id);
-  
-    if (assignment) {
-      const late = isLate(submission.submission.submitted_at, assignment.due_at);
-      const finalScore = calculateScore(submission.submission, assignment, late);
-      //do not round it because every final score will 1
-        console.log(`Learner ID: ${submission.learner_id}, Assignment: ${assignment.name}, Final Score is: ${finalScore}`);
-    }
-  });
+const isLate = (submittedAt, dueAt) => {
+  const submittedDate = new Date(submittedAt);
+  const dueDate = new Date(dueAt);
 
   try {
-    validateCourseAssignment(CourseInfo, AssignmentGroup);
-} catch (error) {
-    console.error(error.message);
+    if (isNaN(submittedDate.getTime()) || isNaN(dueDate.getTime())) {
+    }
+  } catch (error) {
+    console.error("error: ", error.message);
+  }
+  return submittedDate > dueDate;
+};
+
+const calculateScore = (submission, assignment, latePenalty) => {
+  const maxScore = assignment.points_possible;
+  let score = submission.score;
+  if (latePenalty) {
+    score = Math.max(score - maxScore * 0.1, 0);
+  }
+
+  return score / maxScore;
+};
+
+const validateCourseAssignment = (course, assignmentsGrp) => {
+  if (course.id !== assignmentsGrp.course_id) {
+    throw new Error("Wrong Assignment group");
+  }
+  console.log("Validation passed");
+};
+
+function getLearnerData(course, assignmentsGrp, stdSubmission) {
+  validateCourseAssignment(course, assignmentsGrp);
+
+  const learners = stdSubmission.reduce((acc, submission) => {
+    const assignment = assignmentsGrp.assignments.find(
+      (element) => element.id === submission.assignment_id
+    );
+    if (assignment && new Date() >= new Date(assignment.due_at)) {
+      const learnerId = submission.learner_id;
+      if (!acc[learnerId]) {
+        acc[learnerId] = { id: learnerId, finalScore: 0, totalWeight: 0 };
+      }
+
+      const late = isLate(
+        submission.submission.submitted_at,
+        assignment.due_at
+      );
+      const adjustedScore = calculateScore(submission.submission, assignment, late) * assignmentsGrp.group_weight;
+      console.log(
+        `The Learner with ID: ${learnerId}, ` +
+        `Assignment ID: ${assignment.id}, ` +
+        `Was Submitted At: ${submission.submission.submitted_at},\r ` +
+        `The Due At is: ${assignment.due_at},` +
+        `The Late is: ${late},` +
+        `Penalty Applied: ${late ? '\vYes' : '\fNo'}`
+      );
+      
+      acc[learnerId].finalScore += adjustedScore;
+      acc[learnerId].totalWeight += assignmentsGrp.group_weight;
+    }
+    return acc;
+  }, {});
+
+// Calculate final average and format result
+return Object.values(learners).forEach(learner => {
+    const averageScore = learner.totalWeight > 0 ? learner.finalScore / learner.totalWeight : 0;
+    console.log("Learner ID: ", learner.id, "Average Score: ", averageScore.toFixed(2));
+
+});
 }
+getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
-//   learnerEachSubmission(125);
-//   learnerEachSubmission(132);
-
+try {
+  validateCourseAssignment(CourseInfo, AssignmentGroup);
+} catch (error) {
+  console.error(error.message);
+}
